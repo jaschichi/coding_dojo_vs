fb629e1ac964615dfb02dbdd4d2f5ce60503c898
Jasmine Cheng <jasthehippie@gmail.com>
2022-09-13 11:08:04 -0700

    initial commit

---
+++ NOTES_inclass/913morning_session.js
@@ -0,0 +1,55 @@
+// VARIABLES
+// DECLARE A VARIABLES
+var firstName;
+
+ INITIALIZE A VALUE
+firstName = "Edward"
+
+var age = 31
+
+var specificAge = 31.7
+
+var check = false
+
+// consule.log
+// ADDITION
+
+age = age + 1
+age += 1
+age++
+
+// SUBTRACTION
+age = age - 1 
+age -= 1
+age--
+
+// MULTIPLICATION / DIVISION
+age = age / 2
+age /= 2
+
+age = age * 2
+age *= 2
+
+// MODULOUS OPERATOR (REMAINDER)
+age = 33
+console.log(age % 3) 
+
+
+// LOOPS
+// DRY - Don't Repeat Yourself
+
+// for
+
+// initialize a variable ; conditiom ; incretment
+for(var index = 0; index < 100; index++){
+    // CODE BODY 
+    console.log(index)
+}
+
+for(var i = 100; index <= 0; i--){
+    console.log(i)
+}
+
+
+
+// while
